bf79febed3ece0c7572284d32d7fad7f5c5101c4
Hasan <hasank1994@hotmail.com>
2014-04-30 22:59:53 +0300

    23:00 - 30.04.2014 CSS stabilite güncellemesi

---
+++ src/java/load.java
@@ -0,0 +1,15 @@
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Hasan
+ */
+@ManagedBean
+@RequestScoped
+public class load {
+ 
+}
